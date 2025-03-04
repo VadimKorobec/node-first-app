00f52cae05f61c8282ff36a79b18101673963e1c
VadimKorobec <_s_e_t__@ukr.net>
2025-03-04 13:11:40 +0200

    add some code

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Entered Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Click me!</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
 
   res.setHeader("Content-Type", "text/html");
-
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
   res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
   res.write("</html>");
   res.end();
-  res.write();
 });
 
 server.listen(3000);
