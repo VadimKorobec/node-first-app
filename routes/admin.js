556d19bf0ef0c4d74e969d072448680bf67821a5
Vadim <_s_e_t__@ukr.net>
2025-03-08 14:33:41 +0200

    add some code

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,20 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-const adminController = require("../controllers/admin");
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-router.get("/add-product", adminController.getAddProduct);
-router.get("/products", adminController.getProducts);
-router.post("/add-product", adminController.postAddProduct);
-router.put("/edit-product", adminController.editProduct);
-router.delete("/delete-product", adminController.deleteProduct);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 module.exports = router;
