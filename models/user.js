b52e96d2ec70498e77eb37ba5550b00fd63eedcd
Vadim <_s_e_t__@ukr.net>
2025-03-11 16:51:37 +0200

    add cart-item model

--- models/user.js
+++ models/user.js
@@ -11,9 +11,11 @@ const User = sequelize.define("user", {
   },
   name: {
     type: Sequelize.STRING,
+    allowNull:false
   },
   email: {
     type: Sequelize.STRING,
+    allowNull:false
   },
 });
 
