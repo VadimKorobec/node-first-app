dece95ca9a79e8cbf33a9f08ee310a96535cc4b4
Vadim <_s_e_t__@ukr.net>
2025-03-12 15:38:09 +0200

    add some code

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,5 @@
 const Product = require("../models/product");
 
-
 exports.getProducts = (req, res, next) => {
   Product.findAll()
     .then((products) => {
@@ -121,24 +120,34 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
-      req.user.createOrder().then(order => {
-       return order.addProduct(products.map(product => {
-          product.orderItem = {quantity:product.cartItem.quantity}
-          return product
-        }))
-      }).catch(err => {
-        console.log(err)
-      })
+      req.user
+        .createOrder()
+        .then((order) => {
+          return order.addProduct(
+            products.map((product) => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            })
+          );
+        })
+        .catch((err) => {
+          console.log(err);
+        });
       console.log(products);
     })
-    .then(result => {
-      res.redirect('/orders')
+    .then((result) => {
+      return fetchedCart.setProducts(null);
+    })
+    .then((result) => {
+      res.redirect("/orders");
     })
     .catch((err) => {
       console.log(err);
@@ -146,15 +155,17 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout",
-  });
+  req.user
+    .getOrders({include:['products']})
+    .then(orders => {
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders:orders
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  
 };
